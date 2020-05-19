597cbde8178d9c39209447b7af95567edc2b79fe
Dan203 <dan6992@hotmail.com>
2020-05-19 15:53:27 -0700

    Update sign_exes.bat
    Minor update to sign script

--- windows/sign_exes.bat
+++ windows/sign_exes.bat
@@ -9,37 +9,34 @@ setlocal
 
 set SIGNTOOL_PATH="C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x86\signtool.exe"
 set CERT_PATH="C:\Users\Dan\Documents\pyTivoDesktop\pyTivoCert.pfx"
-set TIMESTAMP_SERVER=http://timestamp.comodoca.com/authenticode
+rem set TIMESTAMP_SERVER=http://timestamp.comodoca.com/authenticode
+set TIMESTAMP_SERVER=http://timestamp.verisign.com/scripts/timstamp.dll
 
 
 call "..\sign_password.bat"
 
 
-%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" "pyTivo.exe"
+%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" /d "pyTivo" "pyTivo.exe"
 if %errorlevel% NEQ 0 exit /B %errorlevel%
 
 
-%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" "pyTivoTray.exe"
+%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" /d "pyTivo Desktop Tray" "pyTivoTray.exe"
 if %errorlevel% NEQ 0 exit /B %errorlevel%
 
 
-%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" "pyTivoService.exe"
+%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" /d "pyTivo Service" "pyTivoService.exe"
 if %errorlevel% NEQ 0 exit /B %errorlevel%
 
 
-%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" "desktop/pyTivoDesktop.exe"
+%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" /d "pyTivo Desktop" "desktop/pyTivoDesktop.exe"
 if %errorlevel% NEQ 0 exit /B %errorlevel%
 
 
-%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" "bin/ffmpeg.exe"
+%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" /d "ffmpeg" "bin/ffmpeg.exe"
 if %errorlevel% NEQ 0 exit /B %errorlevel%
 
 
-%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" "bin/tdcat.exe"
-if %errorlevel% NEQ 0 exit /B %errorlevel%
-
-
-%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" "bin/tivolibre.exe"
+%SIGNTOOL_PATH% sign /f %CERT_PATH% /t %TIMESTAMP_SERVER% /p "%SIGNPASS%" /d "TiVo metadata extractor" "bin/tdcat.exe"
 if %errorlevel% NEQ 0 exit /B %errorlevel%
 
 endlocal
